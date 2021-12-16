e8811260a5e28d5e720eef1ab359c08633b9be7b
Marquel <daylensears2@gmail.com>
2021-12-16 09:59:24 -0500

    deployed

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://muffled-rage.herokuapp.com/' : 'http://localhost:3000' 
+
 
 const api = axios.create({
     baseURL: baseURL,
